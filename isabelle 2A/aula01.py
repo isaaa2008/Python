72a483025b234ffd83703229584c70c68f96a61d
Isabelle Lira <isabellelira92@gmail.com>
2024-08-16 11:58:36 -0300

    Add files via upload

---
+++ isabelle 2A/aula01.py
@@ -0,0 +1,9 @@
+nome = "isabelle"
+idade = 16
+
+print("ola",nome,"a sua idade é",idade)
+
+nome ="larissa"
+idade = 15
+altura = 1.60
+print("ola",nome,"a sua idade é",idade, "e sua altura é",altura)
