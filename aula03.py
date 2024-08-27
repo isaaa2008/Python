3a9f3bfa8424f4ae909f3c12c2a4de0ae8be5de0
Isabelle Lira <isabellelira92@gmail.com>
2024-08-27 11:58:34 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,10 @@
+condicão1 = False
+ 
+if(condicão1):
+    print("A condicão 1 é verdadeira")
+   print("outra linha dentro da condicão 1")
+print("fora do if")
+else:
+print("condicão 1 é falsa")
+ print("outra linha dentro da condicão 1")
+print("fora do if")
