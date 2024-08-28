2cc68e903dd190e85ef854101c53d0874a41080e
Isabelle Lira <isabellelira92@gmail.com>
2024-08-28 11:42:51 -0300

    Add files via upload

--- trocavalor.py
+++ trocavalor.py
@@ -1,10 +1,8 @@
 #crie  um algoritimo que armazene dois  valores distintos (A=5 e B=10) e efetue a troca dos valoresde forma que a variavel B passe a possuir o valor da variavel B passe a possuir o valor da variavel B. Por fim, apresentar os valores trocados(A=10 e B=5)
 A = 5
 B = 10
-
-auxiliar = A
-A = B
-
-B = auxiliar
-print("A= ", A)
-print("B= ", B)
+auxiliar=A
+A=B
+B= auxiliar
+print("A= ",A)
+print("B= ",B)
