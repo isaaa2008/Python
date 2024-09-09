e30d7abf0bf87dcb6f7d1562871f01b4bbab6adb
Isabelle Lira <isabellelira92@gmail.com>
2024-09-09 11:01:58 -0300

    Add files via upload

--- 11.tabuada.py
+++ 11.tabuada.py
@@ -1,7 +1,7 @@
 contador = 0 
 numero = int(input("digite o valor da tabuada: "))
 
-while (numero<=10):
+while (contador<=10):
     resultado = numero * contador
     print(f"{numero} x {contador} = {resultado}")
     contador += 1
