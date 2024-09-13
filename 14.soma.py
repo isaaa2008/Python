c46aa79dc056a7415310fb5c57bcabad794e3480
Isabelle Lira <isabellelira92@gmail.com>
2024-09-13 10:53:05 -0300

    Add files via upload

---
+++ 14.soma.py
@@ -0,0 +1,11 @@
+#funções
+#função soma
+def soma(a,b):
+    return a + b
+
+
+valorA=int(input("digite um valor: "))
+valorB=int(input("digite um valor: "))
+
+resultado = soma(valorA,valorB)
+print(f"oresultado da soma ={resultado}")
